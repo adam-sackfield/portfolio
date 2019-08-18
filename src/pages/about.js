0242a22708ba7c438de956034b45daec5f562af2
Adam Sackfield <adampaulsackfield@protonmail.com>
2019-08-18 23:38:30 +0100

    Completed navigation. Completed basic styling and content for Index and About pages. Added font-awesome support.

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,8 +2,73 @@ import React from "react"
 
 import Layout from "../components/Layout/Layout"
 
+import aboutStyles from "./about.module.scss"
+
 const AboutPage = () => {
-  return <Layout>About Page</Layout>
+  return (
+    <Layout>
+      <main id="about">
+        <h1 className={aboutStyles.lgHeading}>
+          About <span className={aboutStyles.textAccent}>Me</span>
+        </h1>
+
+        <h2 className={aboutStyles.smHeading}>A little about me...</h2>
+
+        <div className={aboutStyles.aboutInfo}>
+          <img
+            src="https:/via.placeholder.com/250"
+            alt="Adam Sackfield Bio Image"
+            className={aboutStyles.bioImage}
+          ></img>
+
+          <div className={aboutStyles.bio}>
+            <h3 className={aboutStyles.textAccent}>BIO</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis
+              iste nihil minima repellendus nam, veniam, sunt assumenda
+              perspiciatis tempora, tenetur odio ipsa ratione. A minima
+              laudantium dolor neque quae ratione provident non, aspernatur,
+              facilis omnis labore et. Assumenda fuga qui voluptas dolore id,
+              voluptates, numquam totam dolorem consectetur modi perspiciatis?
+            </p>
+          </div>
+
+          <div className={`${aboutStyles.project} ${aboutStyles.project1}`}>
+            <h3>Example Title</h3>
+            <h6>Main Technology</h6>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae aut
+              vel laudantium sequi inventore ab reiciendis consequatur repellat?
+              Provident enim asperiores doloremque, dicta odio harum minima
+              maxime, ut iste, cumque et vel?
+            </p>
+          </div>
+
+          <div className={`${aboutStyles.project} ${aboutStyles.project2}`}>
+            <h3>Example Title</h3>
+            <h6>Main Technology</h6>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae aut
+              vel laudantium sequi inventore ab reiciendis consequatur repellat?
+              Provident enim asperiores doloremque, dicta odio harum minima
+              maxime, ut iste, cumque et vel?
+            </p>
+          </div>
+
+          <div className={`${aboutStyles.project} ${aboutStyles.project3}`}>
+            <h3>Example Title</h3>
+            <h6>Main Technology</h6>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae aut
+              vel laudantium sequi inventore ab reiciendis consequatur repellat?
+              Provident enim asperiores doloremque, dicta odio harum minima
+              maxime, ut iste, cumque et vel?
+            </p>
+          </div>
+        </div>
+      </main>
+    </Layout>
+  )
 }
 
 export default AboutPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,34 @@ import Helmet from "react-helmet"
 
 import Layout from "../components/Layout/Layout"
 import "./background.scss"
+import indexStyles from "./index.module.scss"
+
+import "../../node_modules/font-awesome/css/font-awesome.min.css"
 
 const IndexPage = () => {
   return (
     <Layout>
       <Helmet bodyAttributes={{ id: "bg-image" }}></Helmet>
 
-      <div id="main">Main</div>
+      <main id="home">
+        <h1 className={indexStyles.lgHeading}>
+          Adam <span className={indexStyles.textAccent}>Sackfield</span>
+        </h1>
+
+        <h2 className={indexStyles.smHeading}>Web Developer / Programmer</h2>
+
+        <div className={indexStyles.icons}>
+          <a href="#!">
+            <i className="fa fa-github fa-2x"></i>
+          </a>
+          <a href="#!">
+            <i className="fa fa-twitter fa-2x"></i>
+          </a>
+          <a href="#!">
+            <i className="fa fa-linkedin fa-2x"></i>
+          </a>
+        </div>
+      </main>
     </Layout>
   )
 }
