f32d17154ead895bb610ff461a6e99345dedc7e2
Adam Sackfield <adampaulsackfield@protonmail.com>
2019-08-18 18:28:01 +0100

    Added SASS and scss modules. Basic styling for navigation implemented. Added JS for menu animation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,130 @@
-import React from "react"
+import React, { Component } from "react"
+import { Link } from "gatsby"
 
-const Header = () => {
-  return <div>Header Component</div>
+import headerStyles from "./Header.module.scss"
+
+class Header extends Component {
+  constructor() {
+    super()
+    this.state = {
+      showMenu: false,
+    }
+  }
+
+  toggleMenu = () => {
+    const currentState = this.state.showMenu
+    console.log(this.state.showMenu)
+    this.setState({ showMenu: !currentState })
+  }
+
+  render() {
+    return (
+      <header>
+        <div
+          className={
+            this.state.showMenu
+              ? `${headerStyles.menuBtn} ${headerStyles.close}`
+              : headerStyles.menuBtn
+          }
+          onClick={this.toggleMenu}
+        >
+          <div className={headerStyles.btnLine}></div>
+          <div className={headerStyles.btnLine}></div>
+          <div className={headerStyles.btnLine}></div>
+        </div>
+
+        <nav
+          className={
+            this.state.showMenu
+              ? `${headerStyles.menu} ${headerStyles.show}`
+              : headerStyles.menu
+          }
+        >
+          <div
+            className={
+              this.state.showMenu
+                ? `${headerStyles.menuBranding} ${headerStyles.show}`
+                : headerStyles.menuBranding
+            }
+          >
+            <div className={headerStyles.portrait}></div>
+          </div>
+
+          <ul
+            className={
+              this.state.showMenu
+                ? `${headerStyles.menuNav} ${headerStyles.show}`
+                : headerStyles.menuNav
+            }
+          >
+            <li
+              className={
+                this.state.showMenu
+                  ? `${headerStyles.navItem} ${headerStyles.show}`
+                  : headerStyles.navItem
+              }
+            >
+              <Link
+                to="/"
+                className={headerStyles.navLink}
+                activeClassName={headerStyles.active}
+              >
+                Home
+              </Link>
+            </li>
+
+            <li
+              className={
+                this.state.showMenu
+                  ? `${headerStyles.navItem} ${headerStyles.show}`
+                  : headerStyles.navItem
+              }
+            >
+              <Link
+                to="/about"
+                className={headerStyles.navLink}
+                activeClassName={headerStyles.active}
+              >
+                About Me
+              </Link>
+            </li>
+
+            <li
+              className={
+                this.state.showMenu
+                  ? `${headerStyles.navItem} ${headerStyles.show}`
+                  : headerStyles.navItem
+              }
+            >
+              <Link
+                to="/projects"
+                className={headerStyles.navLink}
+                activeClassName={headerStyles.active}
+              >
+                Projects
+              </Link>
+            </li>
+
+            <li
+              className={
+                this.state.showMenu
+                  ? `${headerStyles.navItem} ${headerStyles.show}`
+                  : headerStyles.navItem
+              }
+            >
+              <Link
+                to="/contact"
+                className={headerStyles.navLink}
+                activeClassName={headerStyles.active}
+              >
+                Contact Me
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    )
+  }
 }
 
 export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,17 @@
 import React from "react"
+import Helmet from "react-helmet"
+
 import Layout from "../components/Layout/Layout"
+import "./background.scss"
 
 const IndexPage = () => {
-  return <Layout>Index Page</Layout>
+  return (
+    <Layout>
+      <Helmet bodyAttributes={{ id: "bg-image" }}></Helmet>
+
+      <div id="main">Main</div>
+    </Layout>
+  )
 }
 
 export default IndexPage
